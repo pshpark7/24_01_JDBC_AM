645759b085dd312518d1fc651cdeb0c52069a21a
pshpark7 <pshpark07@gmail.com>
2024-01-12 09:03:37 +0900

    게시물 리스트에 검색, 페이징 기능 추가

--- src/com/KoreaIT/java/JDBCAM/App.java
+++ src/com/KoreaIT/java/JDBCAM/App.java
@@ -71,18 +71,18 @@ public class App {
 		MemberController memberController = Container.memberController;
 		ArticleController articleController = Container.articleController;
 
-		if (cmd.equals("member profile")) {
+		if (cmd.equals("member logout")) {
+			memberController.logout();
+		} else if (cmd.equals("member profile")) {
 			memberController.showProfile();
 		} else if (cmd.equals("member login")) {
 			memberController.login();
-		} else if (cmd.equals("member logout")) {
-			memberController.logout();
 		} else if (cmd.equals("member join")) {
 			memberController.doJoin();
 		} else if (cmd.equals("article write")) {
 			articleController.doWrite();
-		} else if (cmd.equals("article list")) {
-			articleController.showList();
+		} else if (cmd.startsWith("article list")) {
+			articleController.showList(cmd);
 		} else if (cmd.startsWith("article modify")) {
 			articleController.doModify(cmd);
 		} else if (cmd.startsWith("article detail")) {
